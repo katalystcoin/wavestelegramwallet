531a87a9882dd0401c633111e50f954e26686305
RJ <me@rjchow.com>
2018-06-18 07:31:54 +0800

    updated config.py

--- config/config_example.py
+++ config/config_example.py
@@ -1,4 +1,6 @@
 BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"
 DB_TABLE_NAME = "MyTelegramWalletBot"
+SECRETS_TABLE_NAME = "MyTelegramWalletBotSecrets"
+DB_ENCRYPTION_KEY_ALIAS = "alias/wavestelegramwalletbot"
 NODE_URL = "https://nodes.wavesplatform.com"
 NET_ID = "mainnet"
